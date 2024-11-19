117030f474629f7761e35ff21d73b41309428326
Nicolas Modrzyk <hellonico@gmail.com>
2024-11-19 18:15:07 +0900

    wasm reminder

---
+++ fahrenheit_to_celsius/static/fahrenheit_to_celsius_wasm_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function run_app(): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_export_3: WebAssembly.Table;
+export function closure93_externref_shim(a: number, b: number, c: number): void;
+export function closure163_externref_shim(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __externref_drop_slice(a: number, b: number): void;
+export function __externref_table_alloc(): number;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
--- fahrenheit_to_celsius_wasm/gen.sh
+++ fahrenheit_to_celsius_wasm/gen.sh
@@ -1,4 +1,12 @@
+https://rustwasm.github.io/wasm-pack/installer/
+
 # don't forget
 # https://stackoverflow.com/questions/74005040/why-i-cannot-find-the-compiled-wasm-file
 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen --out-dir ../fahrenheit_to_celsius/static --target web ../target/wasm32-unknown-unknown/release/frontend.wasm
+
+
+```
+brew install cargo-binstall
+cargo binstall wasm-bindgen-cli
+```
